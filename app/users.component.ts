137b9b119152c8c6b58b5211f6d468cf28991906
Santhoshkumar-p <sanssoft12@gmail.com>
2017-07-15 19:08:25 +0530

    Add files via upload

---
+++ app/users.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+import { UserListComponent } from './user-list.component'
+
+@Component({
+    selector: 'my-users',
+    template:
+        `<user-list></user-list>`
+})
+
+export class UsersComponent {}
+
+   
